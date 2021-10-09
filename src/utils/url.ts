7e33e752e8f2051c87e031b160cf6f5cf09ec36d
iAudreyhepburn <1060695670@qq.com>
2021-10-10 00:03:29 +0800

    chore:改变setSearchParams的实现方式 11-8

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { URLSearchParamsInit, useSearchParams } from "react-router-dom";
 import { cleanObject } from "utils";
 
@@ -6,19 +6,29 @@ import { cleanObject } from "utils";
  * 返回页面url中，指定的参数值
  */
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParam] = useSearchParams();
+  const [searchParams] = useSearchParams();
+  const setSearchParams = useSetUrlSearchParam();
+  const [stateKeys] = useState(keys);
   return [
     useMemo(
       () => keys.reduce((prev, key) => {
         return { ...prev, [key]: searchParams.get(key) || '' }
       }, {} as { [key in K]: string }),
       //eslint-disable-next-line react-hooks/exhaustive-deps
-      [searchParams]
+      [searchParams, stateKeys]
     ),
     (params: Partial<{ [key in K]: unknown }>) => {
-      const o = cleanObject({ ...Object.fromEntries(searchParams), ...params }) as URLSearchParamsInit;
-      return setSearchParam(o);
+      return setSearchParams(params)
     }
     // setSearchParam
   ] as const
+}
+
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParam] = useSearchParams();
+
+  return (params: { [key in string]: unknown }) => {
+    const o = cleanObject({ ...Object.fromEntries(searchParams), ...params }) as URLSearchParamsInit;
+    return setSearchParam(o);
+  }
 }
